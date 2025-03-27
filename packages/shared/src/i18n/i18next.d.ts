a5f196b852ffdea5845fa1ca35af2dffc8256ace
Socolov Alexandr <82725768+assassin0210@users.noreply.github.com>
2025-03-27 18:15:38 +0200

    add declare types for translate

--- packages/shared/src/i18n/i18next.d.ts
+++ packages/shared/src/i18n/i18next.d.ts
@@ -3,7 +3,6 @@ import translation from '../locales/en/translation.json';
 
 declare module 'i18next' {
   interface CustomTypeOptions {
-    defaultNS: 'translation';
     resources: {
       translation: typeof translation;
     };
--- packages/shared/src/i18n/index.ts
+++ packages/shared/src/i18n/index.ts
@@ -1,8 +1,8 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+
 import en from '../locales/en/translation.json';
 import ru from '../locales/ru/translation.json';
-import './i18next';
 
 const resources = {
   en: { translation: en as typeof en },
