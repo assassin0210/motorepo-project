2485f983d577abcaea8b8fc9f5a472a04ba19519
Socolov Alexandr <82725768+assassin0210@users.noreply.github.com>
2025-03-27 22:13:16 +0200

    trigger

--- packages/shared/src/i18n/index.ts
+++ packages/shared/src/i18n/index.ts
@@ -2,6 +2,7 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 import en from '../locales/en/translation.json';
+
 import ru from '../locales/ru/translation.json';
 
 const resources = {
